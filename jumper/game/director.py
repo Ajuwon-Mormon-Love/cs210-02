807d9a21e211110a7f63438e8fe67c8c5c36125c
Mike Lewis <wyoming.c64@gmail.com>
2022-05-25 15:07:56 -0600

    Unified method calls to other classes.

--- jumper/game/director.py
+++ jumper/game/director.py
@@ -1,13 +1,28 @@
 from game.terminal_service import TerminalService
 from game.skydiver import Skydiver
 from game.dictionary import Dictionary
+from game.player import Player
 
 class Director:
-    """A person who directs the game. 
+    """A person or thing that directs the game. 
     
     The responsibility of a Director is to control the sequence of play.
     
     Attributes:
+        _skydiver (Skydiver): An instance of Skydiver.
+        _dictionary (Dictionary): An instance of Dictionary.
+        _player (Player): An instance of Player.
+        _term (Terminal_Service): An instance of Terminal_Service.
+        _is_playing (boolean): Whether we are still playing the game or not.
+        _evaluation: A variable to hold the results of any evaluations.
+
+    Methods:
+        __init__(self): Constructor for an instance of Director.
+        start_game(self): Starts the game and runs the loop until we are not playing anymore.
+        _end_game(self): Handles end-of-game to display some appropriate messages.
+        _get_inputs(self): The get input phase of a round.
+        _do_updates(self): The update statuses phase of the round.
+        _do_outputs(self): The display outputs phase of the round.
         
     """
     def __init__(self):
@@ -18,9 +33,9 @@ class Director:
         """
         self._skydiver = Skydiver()
         self._dictionary = Dictionary()
-        self._terminal_service = TerminalService()
+        self._player = Player()
+        self._term = TerminalService()
         self._is_playing = True
-        self._evaluation = True
 
 
     def start_game(self):
@@ -29,24 +44,53 @@ class Director:
         Args:
             self (Director): an instance of Director.
         """
-        self._skydiver.draw_figure()
-
-        while self._is_playing:
+        while self._is_playing:   
+            self._do_outputs()
             self._get_inputs()
             self._do_updates()
-            self._do_outputs()
+
+        self._end_game()
+
+
+    def _end_game(self):
+        """Handles the ending of the game: Either we saved the jumpers life, or we didn't.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        if self._dictionary.is_word_complete():
+            self._term.write_text("Congratulations! You saved the skydiver!")
+        if self._skydiver.is_dead():
+            self._term.write_text("Oh no! The skydiver lost his parachute and didn't survive.")
+            self._dictionary.display_mystery_word(self._term)
     
 
     def _get_inputs(self):
-        new_guess = self._terminal_service.read_text("\nGuess a letter [a-z]: ")
-        self._evaluation = self._dictionary.evaluate_guess(new_guess)
+        """The get input phase of the round.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        self._player.set_guess(self._term.read_text("\nGuess a letter [a-z]: "))
 
 
     def _do_updates(self):
-        self._skydiver.check_status(self._evaluation)
+        """The update statuses phase of the round.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        self._dictionary.check_guess(self._player)
+        self._is_playing = self._dictionary.is_word_complete() or self._skydiver.is_dead()
+        
 
     def _do_outputs(self):
-        self._dictionary.update_letters(self._dictionary._current_letter)
+        """The display outputs phase of the round.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
         self._skydiver.draw_figure()
-        if self._skydiver.is_dead_status():
-            self._is_playing = False
+        self._term.write_text(self._dictionary.get_revealed_word)
+
+        
